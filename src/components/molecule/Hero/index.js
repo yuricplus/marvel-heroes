bca52cf9c2ad05f23296b91f5b6b2e85431ebb14
Yuri Almeida <yuri.almeida@dotz.com>
2021-05-01 12:28:48 -0300

    Feature: card of hero

--- src/components/molecule/Hero/index.js
+++ src/components/molecule/Hero/index.js
@@ -1,15 +1,42 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { Link } from 'react-router-dom'
 
 import Card from '../../atom/card'
 import Icon from '../../atom/Icon'
 
+import './style.scss'
+
 const Hero = (props) => {
-  const { name } = props
+  const { heroData, image, favHero } = props
+  const [liked, setLiked] = useState([])
+  const [update, setUpdate] = useState(false)
+
+  useEffect(() => {
+    setLiked(JSON.parse(localStorage.getItem('listFavStorage')) || [])
+  }, [update])
+
   return (
-    <div>
-      <Card />
-      <p>{name}</p>
-      <Icon name="heart" />
+    <div className="hero-box">
+      <Link to={`/hero-details/${heroData.id}`}>
+        <Card image={image} text={heroData.name} />
+      </Link>
+      <div className="content-info">
+        <p className="text bold">{heroData.name}</p>
+        <button
+          className="btn-transparent"
+          type="button"
+          onClick={() => {
+            setLiked(JSON.parse(localStorage.getItem('listFavStorage')) || [])
+            setUpdate(true)
+            favHero(heroData.id)
+          }}
+        >
+          <Icon
+            name={liked.includes(heroData.id) ? 'heart-liked' : 'heart'}
+            click="pointer"
+          />
+        </button>
+      </div>
     </div>
   )
 }
